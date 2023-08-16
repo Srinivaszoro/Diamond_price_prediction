e731244216efab753b0abf4401b5528a2f9c9076
Srinivaszoro <srinivasmarella235@gmail.com>
2023-08-16 23:41:25 +0530

    utilis and data_ingestion and data_tranformation

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import pandas as pd
+
+from src.Exception import CustomException
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from src.components.data_tranformation import DataTrafromation
+from src.logger import logging
+
+
+#Intitialize the Data Ingestion Configuration
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str=os.path.join('artifacts','train.csv')
+    test_data_path:str=os,path.join('artifacts','test.csv')
+    raw_data_path:str=os.path.join('artifacts','raw.csv')
+
+#create a class for data ingestion
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+
+    def Initiate_data_Ingestion(self):
+        logging.info('data ingestion step started')
+        try:
+            df=pd.read_csv("https://raw.githubusercontent.com/Srinivaszoro/FSDSRegression/main/notebooks/data/gemstone.csv")
+            logging.info('data read as pandas dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False)
+            logging.info('Train test split started')
+            train_set,test_set=train_test_split(df,test_szie=0.30,random_state=30)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info('Data ingestion completed')
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+        except Exception as e:
+            logging.info('Exception occured at Data Ingestion Stage')
+            raise CustomException(e,sys)
+
+
+## run data ingestion
+
+if __name__=='__main__':
+    obj=DataIngestion()
+    train_data_path,test_data_path=obj.Initiate_data_Ingestion()
+    data_tranformation=DataTrafromation()
+    train_arr,test_arr,_=data_tranformation.initaite_data_transformation(train_data_path,test_data_path)
+
+    
---
+++ src/components/data_tranformation.py
@@ -0,0 +1,114 @@
+import sys
+import pandas as pd
+import numpy as np 
+
+from src.Exception import CustomException
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import pipeline
+from sklearn.preprocessing import OrdinalEncoder,StandardScaler
+import os
+from dataclasses import dataclass
+from src.utilis import save_object
+from src.logger import logging
+@dataclass
+class DataTranformatiomConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
+
+class DataTrafromation:
+    def __init__(self):
+        self.data_tranformation_config=DataTranformatiomConfig()
+
+    def get_data_transformation_object(self):
+        try:
+            logging.info('Data tranformatiopn initiated')
+            #define categorical colomns and numerical colomns
+            categorical_cols=['cut','color','clarity']
+            numerical_cols=['carat','depth','table','x','y','z']
+
+            #define custom ranking for categorical colomns
+
+            cut_categories=['Fair','Good','Very Good','Premium','Ideal']
+            color_categories=['D','E','F','G','H','I','J']
+            clarity_categories=['I1','SI2','SI1','VS2','VS1','VVs2','VVS1','IF']
+
+            logging.info('Pipeline initiated')
+
+            ##numerical Pipeline
+            num_pipeline=Pipeline(
+                steps=[
+                    ('imputer',SimpleImputer(strategy='median')),
+                    ('scaler',StandardScaler())
+
+                ]
+            )
+
+            ##categorical Pipeline
+            cat_pipeline=Pipeline(
+                steps=[
+                    ('imputer',SimpleImputer(strategy='most_frequent')),
+                    ('Ordinalencoder',OrdinalEncoder(categories=[cut_categories,color_categories,clarity_categories])),
+                    ('scaler',StandardScaler())
+                ]
+            )
+
+            preprocessor=ColumnTranformer([
+                ('num_pipeline',num_pipeline,numerical_cols),
+                ('cat_pipeline',cat_pipeline,categorical_cols)
+            ])
+
+            logging.info('Pipeline completed')
+
+
+        except Exception as e:
+            logging.info('Error in get data transformation')
+            raise CustomException(e,sys)
+
+
+    def initaite_data_transformation(self,train_path,test_path):
+        try:
+            # Reading train and test data
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info('Read train and test data completed')
+            logging.info(f'Train Dataframe Head : \n{train_df.head().to_string()}')
+            logging.info(f'Test Dataframe Head  : \n{test_df.head().to_string()}')
+
+            logging.info('onbtaining preprocessor object')
+
+            preprocessing_obj=self.get_data_transformation_object()
+
+            target_coloumn_name='price'
+            drop_columns=[target_coloumn_name,'id']
+
+            input_feature_train_df=train_df.drop(columns=drop_columns,axis=1)
+            target_feature_train_df=train_df[target_coloumn_name]
+
+            input_feature_test_df=test_df.drop(columns=drop_columns,axis=1)
+            target_feature_test_df=test_df[target_coloumn_name]
+
+            ##tranformating using preprocessor obj
+            input_feature_train_arr=preprocessing_obj.fit_tranform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.fit_tranform(input_feature_test_df)
+
+            logging.info('Applying preprocessing obj on traing and testing datasets')
+
+            train_arr=np.c_[input_feature_rain_arr, np.array(target_feature_train_df)]
+            test_arr=np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            save_object(
+                file_path=self.data_tranformation_config.preprocessing_obj_file_path,
+                obj=preprocessing_obj
+            )
+
+            logging.info('preprocessor pickel file saved')
+
+            return(
+                train_arr,
+                test_arr,
+                self.data_tranformation_config.preprocessing_obj_file_path
+            )
+
+        except Exception as e:
+            logging.info('exception occured in the initiate tranformation cell')
+            raise CustomException(e,sys)
